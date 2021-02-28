23045981d182040d1ff43baa7d105a51ea85d952
Administrator <abin@unisissolutions.com>
2021-02-28 15:50:12 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
-$config = parse_ini_file('/Users/admin/Sites/to-do.ini');
+$config = parse_ini_file('/API/to-do.in');
 $conn = mysqli_connect($config['dbhost'], $config['username'], $config['password']);
 mysqli_select_db($conn, $config['db']);
